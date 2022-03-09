e3c0ad2270f1adbf79cd89606b54b6871a71d73b
Danilo <danilogarces19801@gmail.com>
2022-03-09 20:50:31 -0300

    adicionar compra-venda

--- bot_bitcoin.py
+++ bot_bitcoin.py
@@ -1,12 +1,28 @@
 import websocket
-
+import bitstamp.client
 import json
+import credentials
+
+def client():
+    trading_client = bitstamp.client.Trading(username=credentials.USERNAME, key=credentials.KEY, secret=credentials.SECRET)
+    return trading_client
+
+
+def buy(quantidade):
+    trading_client = client()
+    trading_client.buy_market_order(amount=quantidade)
+
+
+def sell(quantidade):
+    trading_client = client()
+    trading_client.sell_market_order(amount=quantidade)
 
 
 def on_message(ws, message):
 
     message = json.loads(message)
     print(message["data"]["price"])
+
 
 def on_open(ws):
     print("conexão aberta\n")
